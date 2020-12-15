9201087faf2ead49650e27d76876642d9bd03ce2
zouzonghua <zouzonghua0511@gmail.com>
2020-12-15 17:42:06 +0800

    style: 格式化代码

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { NativeModules } from 'react-native';
 import { Minder, emitter, command } from './src/minder';
-const { MindPlugin } = NativeModules;
 
-module.exports.MindPlugin = MindPlugin;
 module.exports.Minder = Minder;
 module.exports.emitter = emitter;
 module.exports.command = command;
